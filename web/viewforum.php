83cacb159feb611b5bfc834ba183fa239c3ade90
Alexandr <258428@mail.ru>
2015-03-23 16:20:28 +0700

    add 301 code to the header string

--- web/viewforum.php
+++ web/viewforum.php
@@ -2,6 +2,7 @@
 // /viewforum.php?id=XXX to /forum/XXX
 $uri = $_SERVER['REQUEST_URI'];
 preg_match('/viewforum\.php\?id=(\d+)/', $uri, $matches);
+header('HTTP/1.1 301 Moved Permanently');
 header("Location: http://" . $_SERVER['HTTP_HOST'] . "/forum/" . $matches[1]);
 die();
 
--- web/viewtopic.php
+++ web/viewtopic.php
@@ -1,6 +1,7 @@
 <?php
 // /viewtopic.php?id=XXX or /viewtopic.php?pid=XXX to /topic/XXX
 $uri = $_SERVER['REQUEST_URI'];
+header('HTTP/1.1 301 Moved Permanently');
 if(strpos($uri, 'pid')) {
     preg_match('/viewtopic\.php\?pid=(\d+)/', $uri, $matches);
     header("Location: http://" . $_SERVER['HTTP_HOST'] . "/post/" . $matches[1]);
