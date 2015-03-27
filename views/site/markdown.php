5fcb455e6d3985ed6e2f0fde9774c83b4934c70b
Alexandr <258428@mail.ru>
2015-03-27 23:51:39 +0700

    update copyrights

--- views/site/markdown.php
+++ views/site/markdown.php
@@ -2,6 +2,7 @@
 /* @var \app\models\Post $post */
 
 $this->title = 'Руководство по использованию Markdown';
+$this->subtitle = 'составитель руководства <a href="/user/31059">kirillov3798</a>';
 ?>
 <div class="markdown-body">
     <?= $post->displayMessage ?>
