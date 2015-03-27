d2dd41ab524fac0cdf2fbf55c9e8514abf175cd5
Alexandr <258428@mail.ru>
2015-03-27 23:14:54 +0700

    return headers block in parser

--- helpers/MarkdownParser.php
+++ helpers/MarkdownParser.php
@@ -4,13 +4,5 @@ namespace app\helpers;
 
 class MarkdownParser extends \Parsedown
 {
-    protected function blockHeader($Line)
-    {
-        return;
-    }
 
-    protected function blockSetextHeader($Line, array $Block = null)
-    {
-        return;
-    }
 }
