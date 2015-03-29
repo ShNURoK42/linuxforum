8fe2776da8c9a239fe9358366903842cae2824a3
Alexandr <258428@mail.ru>
2015-03-29 22:54:51 +0700

    Added bbcode color tag

--- helpers/MarkdownParser.php
+++ helpers/MarkdownParser.php
@@ -4,5 +4,27 @@ namespace app\helpers;
 
 class MarkdownParser extends \Parsedown
 {
+    function __construct()
+    {
+        $this->InlineTypes['['] []= 'ColoredText';
 
+        $this->inlineMarkerList .= '[';
+    }
+
+    protected function inlineColoredText($Excerpt)
+    {
+        if (preg_match('/\[color=([a-zA-Z]{3,20}|\#[0-9a-fA-F]{6}|\#[0-9a-fA-F]{3})](.*?)\[\/color\]/', $Excerpt['text'], $matches))
+        {
+            return array(
+                'extent' => strlen($matches[0]),
+                'element' => array(
+                    'name' => 'span',
+                    'text' => $matches[2],
+                    'attributes' => array(
+                        'style' => 'color: '.$matches[1],
+                    ),
+                ),
+            );
+        }
+    }
 }
