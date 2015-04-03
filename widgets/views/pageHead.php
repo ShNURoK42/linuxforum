5350860f6cbbe870e91b738801f5f49fa327d43d
Alexandr <258428@mail.ru>
2015-04-04 01:50:24 +0700

    Fixes #4: XSS protection

--- widgets/views/pageHead.php
+++ widgets/views/pageHead.php
@@ -2,10 +2,12 @@
 /* @var string $title */
 /* @var string $subtitle */
 /* @var array $options */
+
+$formatter = Yii::$app->formatter;
 ?>
 <div class="<?= $options['class'] ?>">
     <div class="container">
-        <h2><?= $title ?></h2>
+        <h2><?= $formatter->asText($title) ?></h2>
         <?php if (isset($subtitle)): ?>
         <p><?= $subtitle ?></p>
         <?php endif; ?>
