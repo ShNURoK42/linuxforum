2322cff7dfee0510999f08528f6f3887df34fc8d
Alexandr <258428@mail.ru>
2015-04-24 14:02:29 +0700

    fix online list

--- modules/frontend/Module.php
+++ modules/frontend/Module.php
@@ -2,6 +2,9 @@
 
 namespace frontend;
 
+use Yii;
+use user\models\UserOnline;
+
 class Module extends \yii\base\Module
 {
     public $controllerNamespace = 'frontend\controllers';
@@ -10,6 +13,28 @@ class Module extends \yii\base\Module
     {
         parent::init();
 
-        // custom initialization code goes here
+        $ip = ip2long(Yii::$app->getRequest()->getUserIP());
+
+        $online = UserOnline::find()
+            ->where(['user_ip' => $ip])
+            ->one();
+
+        if (!$online instanceof UserOnline) {
+            $online = new UserOnline();
+        }
+
+        $online->vizited_at = time();
+        $online->user_ip = $ip;
+
+        if (!Yii::$app->getUser()->getIsGuest()) {
+            $online->user_id = Yii::$app->getUser()->getIdentity()->getId();
+        } else {
+            $online->user_id = 0;
+        }
+
+        $online->save();
+        UserOnline::deleteInactiveUsers();
+
+        return true;
     }
 }
