13fcf1704e96668a0cd5a9bc9eb75ad54fedf8a9
Alexandr <258428@mail.ru>
2015-04-27 18:45:19 +0700

    fix error with wrong email sent

--- modules/notify/Module.php
+++ modules/notify/Module.php
@@ -47,17 +47,17 @@ class Module extends \yii\base\Module
 
                 $currentUser = Yii::$app->getUser()->getIdentity();
                 $model = new UserMention();
+                $model->user_id = $currentUser->id;
+                $model->mention_user_id = $mentioned->id;
+                $model->post_id = $post->id;
+                $model->topic_id = $post->topic->id;
+                $model->status = UserMention::MENTION_SATUS_UNVIEWED;
 
-                if ($currentUser->notify_mention_web == 1) {
-                    $model->user_id = $currentUser->id;
-                    $model->mention_user_id = $mentioned->id;
-                    $model->post_id = $post->id;
-                    $model->topic_id = $post->topic->id;
-                    $model->status = UserMention::MENTION_SATUS_UNVIEWED;
+                if ($mentioned->notify_mention_web == 1) {
                     $model->save();
                 }
 
-                if ($currentUser->notify_mention_email == 1) {
+                if ($mentioned->notify_mention_email == 1) {
                     \Yii::$app->mailer->compose(['text' => 'mention'], [
                         'model' => $model,
                         'topic' => $post->topic,
