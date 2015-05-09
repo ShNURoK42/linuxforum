0b2953639117ddfeae29c09944b4858e68815636
Alexandr <258428@mail.ru>
2015-05-09 17:50:58 +0700

    Update ads module

--- modules/ad/Ad.php
+++ modules/ad/Ad.php
@@ -5,12 +5,14 @@ use Yii;
 
 class Ad extends \yii\base\Widget
 {
+    public $domen = 'linuxforum.ru';
+
     /**
      * @inheritdoc
      */
     public function run()
     {
-        if (Yii::$app->getUser()->getIsGuest()) {
+        if (Yii::$app->getUser()->getIsGuest() && stristr($_SERVER['HTTP_HOST'], $this->domen)) {
             $view = $this->getView();
             AdAsset::register($view);
 
